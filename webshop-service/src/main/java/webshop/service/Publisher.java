59bcafe9b5518d21203475da75f330edf0cccf26
dirk z <dirk.v.zon@outlook.com>
2021-01-03 11:15:45 +0100

    edited publisher

--- webshop-service/src/main/java/webshop/service/Publisher.java
+++ webshop-service/src/main/java/webshop/service/Publisher.java
@@ -1,9 +1,7 @@
 package webshop.service;
 
-import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 
-import java.io.IOException;
 import java.net.URI;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,9 +21,7 @@ class Publisher {
             // create and start a grizzly server
             GrizzlyHttpServerFactory.createHttpServer(BASE_URI, customApplicationConfig, true);
 
-            Logger.getGlobal().log(Level.CONFIG, "Hosting resources at " + BASE_URI.toURL());
-
-        } catch (IOException ex) {
+        } catch (Exception ex) {
             Logger.getLogger(Publisher.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
